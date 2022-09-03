5a86e0952ac3c36bdf97fb16b8486c834cf23666
neşet mehyedin <nachaat.com@gmail.com>
2022-09-03 22:11:13 +0300

    components.

---
+++ src/components 1/Text.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+ function Text() {
+   {client == "Designer" ? title.designer : title.programmer} web.
+     return(
+        <p>Graphic and web Design are far more than a job for me. thanks to me extensive
+        technical knowledge, I can implement my creative ideas very well, so that I can 
+        offer tailor-made solutions for customers. </p>
+     );
+}
+export default Text;
